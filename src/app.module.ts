e1eb8d1f83487cbf35fbabc5937f740aaf9f74a3
Edwin Edjokpa <edjokpaedwin@gmail.com>
2025-01-29 03:57:04 +0100

    added ping service due to render sleep

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PingService } from './ping/ping.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PingService],
 })
 export class AppModule {}
